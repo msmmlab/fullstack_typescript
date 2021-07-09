b9f9169d10ff62ba4294bb0b2ac0a3d9549b8232
Marcin Stepien <ms@mmlab.ie>
2021-07-09 21:42:22 +1000

    grapgql setup

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,36 @@
 import "reflect-metadata";
 import { MikroORM } from "@mikro-orm/core";
-import { Post } from "./entities/Post";
+// import { Post } from "./entities/Post";
 import mikroConfig from "./mikro-orm.config";
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { HelloResolver } from "./resolvers/hello";
 
 const main = async () => {
   const orm = await MikroORM.init(mikroConfig);
   await orm.getMigrator().up();
-  // const post = orm.em.create(Post, { title: "My first post message" });
-  // await orm.em.persistAndFlush(post);
 
-  const posts = await orm.em.find(Post, {});
-  console.log(posts);
+  const app = express();
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver],
+      validate: false,
+    }),
+  });
+
+  apolloServer.applyMiddleware({ app });
+
+  app.get("/", (_, res) => {
+    res.status(200).send("Hello");
+  });
+
+  app.listen(3030, () => {
+    console.log("Server running on PORT:3030");
+  });
 };
 
 main()
-  .then(() => console.log("Running..."))
+  .then(() => console.log("Setup: passed"))
   .catch(console.error);
